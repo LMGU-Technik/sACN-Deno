7a634b7e3594c588ab9e7f1be61dff8560e81c23
Hans Schallmoser <99032404+hansSchall@users.noreply.github.com>
2023-11-20 16:42:31 +0100

    move to deno basics

--- src/util.ts
+++ src/util.ts
@@ -1,22 +1,8 @@
-/** Channel 1 is index 1. There is no index 0. The value range is `0-100` */
-export type Payload = { [channel: number]: number };
-
 export function multicastGroup(universe: number): string {
   if ((universe > 0 && universe <= 63999) || universe === 64214) {
     return `239.255.${universe >> 8}.${universe & 255}`;
   }
   throw new RangeError('universe must be between 1-63999');
-}
-
-export const dp = (n: number, decimals = 2): number =>
-  Math.round(n * 10 ** decimals) / 10 ** decimals;
-
-export function objectify(buf: Buffer): Payload {
-  const data: Payload = {};
-  buf.forEach((val, ch) => {
-    if (val > 0) data[ch + 1] = dp(val / 2.55, 2); // rounding to 2dp will not lose any data
-  });
-  return data;
 }
 
 export const inRange = (n: number): number =>
